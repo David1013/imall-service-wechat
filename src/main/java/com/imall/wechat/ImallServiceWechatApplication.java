e4dc5be34d3dc4597f4c03ae2ea85c4b06f0c499
王学伟 <53150807@qq.com>
2019-02-02 09:20:21 +0800

    初始提交

---
+++ src/main/java/com/imall/wechat/ImallServiceWechatApplication.java
@@ -0,0 +1,14 @@
+package com.imall.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImallServiceWechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImallServiceWechatApplication.class, args);
+	}
+
+}
+
